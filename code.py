c3955b45a81867a118813d2f763ac1268073de10
emreatmaca03 <117234290+emreatmaca03@users.noreply.github.com>
2024-11-05 19:22:58 +0300

    Updating file code.py

--- code.py
+++ code.py
@@ -1,1 +1,7 @@
-print('Hello from the new branch!')
+# CS 421 Programming Assignment 1 - PseudoGit
+
+def greet_the_student():
+    return "22102255"
+
+if(__name__ == "__main__"):
+    print(greet_the_student())
